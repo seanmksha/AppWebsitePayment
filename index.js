9f2903fd02070ef64e9f986f0504519c10e67091
Sean Sharma <seanmakosharma@gmail.com>
2017-09-17 01:55:47 -0700

    changed greeting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.send({hi:'there'})
+    res.send({bye:'buddy'})
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
